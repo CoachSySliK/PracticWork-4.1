d68309bae8ee8e1a23fb74150f729450c843a992
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-05 22:04:00 +0300

    Добавьте файлы проекта.

---
+++ PracticWork-4.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cout << "Сравниваем два числа\n\n";
+    cout << "Введите первое число: ";
+    cin >> x;
+    cout << "Введите второе число: ";
+    cin >> y;
+
+    if (x < y) {
+        cout << x << " меньше чем " << y << endl << endl;
+	}
+	else if (x > y) {
+        cout << x << " больше чем " << y << endl << endl;
+	}
+	else {
+        cout << x << " равно " << y << endl << endl;
+    }
+    
+    return 0;
+}
